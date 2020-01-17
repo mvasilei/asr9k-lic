eba7f72455bcd27aa045f8b5e93b9b00f4ca7aea
mvasilei <53435274+mvasilei@users.noreply.github.com>
2020-01-17 14:36:46 +0000

    Update asr9k-lic.py

--- asr9k-lic.py
+++ asr9k-lic.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 from datetime import datetime, timedelta
-import subprocess, re
+import subprocess, re, signal
+
+def signal_handler(sig, frame):
+    print('Exiting gracefully Ctrl-C detected...')
+    sys.exit(0)
 
 def main():
     try:
@@ -9,13 +13,17 @@ def main():
     except IOError:
         print 'Could not read file hosts'
 
-    pattern = re.compile(r'^(?P<module>LC\/0\/[0-9]\/CPU0).*(?P<error>LICENSE)')
-    pe = re.compile(r'ukx[a-z]{2}[1-9]pe[0-1][1-9]')
+    # match the log line that contain LICENSE and also matches on LC the result is directed to groups called module and error
+    pattern = re.compile(r'^(?P<module>LC\/0\/[0-9]\/CPU0).*(?P<error>LICENSE)', re.MULTILINE)
+    # match on the MPEs and IGW hostnames
+    pe = re.compile(r'ukx[a-z]{2}[1-9][ap][be][0-1][1-9]')
     igw = re.compile(r'[a-z]{4}[0-9]{2}-igw-a1')
 
     yesterday = datetime.strftime(datetime.now() - timedelta(1), ' %b %d ')
     flag = 0
 
+    '''for each line read from the /etc/hosts file if is MPE or IGW execute show log start 'yesterday' and if
+       if successfully executed search for LICENSE. If exist then create a swap file and store the information'''
     for host in lines:
         if (re.findall(pe, host.lower()) or re.findall(igw, host.lower())):
             ip, name = host.split()
@@ -25,9 +33,7 @@ def main():
                                         shell=True)
 
             if response.returncode == None :
-                m = pattern.match(response.communicate()[0].strip('\n'))
-
-                if m:
+                for m in pattern.finditer(response.communicate()[0].strip('\n')):
                     if flag == 0:
                         with open('swap','w') as f:
                             f.write('Host:' + name + ' Module: ' + m.group('module') + ' needs Licensing\n')
@@ -39,9 +45,11 @@ def main():
             else:
                 print 'An error occurred'
 
+    # if there had been unlicensed module send email with the information stored in swap file
     if flag == 1:
             response = subprocess.Popen(['mailx -s "Unlicensed ASR9K Nodes" IPMobileCore@vodafone.com < swap'],
                                         stdout=subprocess.PIPE,
                                         shell=True)
 if __name__ == '__main__':
-   main()
+    signal.signal(signal.SIGINT, signal_handler)  # catch ctrl-c and call handler to terminate the script
+    main()
