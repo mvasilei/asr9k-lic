a0ea2ea8f119ec3ca6a5c00dcb254dd127c83faa
mvasilei <53435274+mvasilei@users.noreply.github.com>
2020-01-20 08:01:42 +0000

    Update asr9k-lic.py

--- asr9k-lic.py
+++ asr9k-lic.py
@@ -16,7 +16,7 @@ def main():
     # match the log line that contain LICENSE and also matches on LC the result is directed to groups called module and error
     pattern = re.compile(r'^(?P<module>LC\/0\/[0-9]\/CPU0).*(?P<error>LICENSE)', re.MULTILINE)
     # match on the MPEs and IGW hostnames
-    pe = re.compile(r'ukx[a-z]{2}[1-9][ap][be][0-1][1-9]')
+    pe = re.compile(r'ukx[a-z]{2}[1-9][ap][be][0-1][1-9]|ukny[ac]1pe0[9]')
     igw = re.compile(r'[a-z]{4}[0-9]{2}-igw-a1')
 
     yesterday = datetime.strftime(datetime.now() - timedelta(1), ' %b %d ')
@@ -37,19 +37,18 @@ def main():
                     if flag == 0:
                         with open('swap','w') as f:
                             f.write('Host:' + name + ' Module: ' + m.group('module') + ' needs Licensing\n')
+                        flag = 1
                     else:
                         with open('swap','a') as f:
                             f.write('Host:' + name + ' Module: ' + m.group('module') + ' needs Licensing\n')
-
-                    flag = 1
             else:
                 print 'An error occurred'
 
     # if there had been unlicensed module send email with the information stored in swap file
     if flag == 1:
-            response = subprocess.Popen(['mailx -s "Unlicensed ASR9K Nodes" IPMobileCore@vodafone.com < swap'],
+            '''response = subprocess.Popen(['mailx -s "Unlicensed ASR9K Nodes" IPMobileCore@vodafone.com < swap'],
                                         stdout=subprocess.PIPE,
-                                        shell=True)
+                                        shell=True)'''
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)  # catch ctrl-c and call handler to terminate the script
     main()
